88fd400ded4e79e70b002a3ff3fa5f01502ef3c7
moataz-armash <mo3taz314@gmail.com>
2024-04-01 17:53:53 +0300

    make game logic and design Game Board

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+const Player = ({ initialName, symbol, isActive }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(true);
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+};
+
+export default Player;
